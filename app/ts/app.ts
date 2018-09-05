3e6f81a28fe5fb54f0e2415d6c9bfd9ed18d5e41
Yuri <yuri.zkt@gmail.com>
2018-09-05 13:33:05 -0300

    Elaborando uma solução de view

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,5 @@
-const negociacao = new Negociacao(new Date(), 1, 100);
+const controller = new NegociacaoController();
 
-console.log(negociacao);
+document
+    .querySelector('.form')
+    .addEventListener('submit', controller.adiciona.bind(controller));
