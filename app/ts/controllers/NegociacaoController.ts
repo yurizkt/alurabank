925a2767300b7cba6950b3b658b7e56c673beb09
Yuri <yuri.zkt@gmail.com>
2018-09-05 15:09:18 -0300

    Utilizando herança nas classes de Views e abstraindo a classe e método

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -5,6 +5,7 @@ class NegociacaoController{
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
+    private _mensagemview = new MensagemView('#mensagemView')
 
     constructor(){
         this._inputData = <HTMLInputElement>document.querySelector('#data');
@@ -26,5 +27,6 @@ class NegociacaoController{
         this._negociacoes.adiciona(negociacao);
         
         this._negociacoesView.update(this._negociacoes);
+        this._mensagemview.update('Negociação adicionada com sucesso.')
     }
 }
