89106933a5f3c0096b6f1f44c8734e35be592d3e
Yuri <yuri.zkt@gmail.com>
2018-09-06 11:45:11 -0300

    passando paramentro para escapar o código como opicional

--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -3,6 +3,6 @@ export class Negociacao{
     constructor(readonly data: Date, readonly quantidade: number, readonly valor: number){}
 
     get volume(){
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
     }
 }
